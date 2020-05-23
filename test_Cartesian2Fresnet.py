843fcc7d537306ea5a3a12d31b1c7ddf0f63b0c4
Bruno Justino Garcia Praciano <bjpraciano@gmail.com>
2020-05-23 19:18:30 +0200

    Writing the test for test_reference_spline

--- test_Cartesian2Fresnet.py
+++ test_Cartesian2Fresnet.py
@@ -1,10 +1,32 @@
 import Cartesian2Fresnet
 import unittest
 
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
+import os
+
 class TestFresnet(unittest.TestCase):
-    """
-    Test the add function from the mymath library
-    """
+
+    def setUp(self):
+        dictpat = r'\{((?:\s*\w+\s*:\s*\D+\w+.\d+\s*)+)\}' 
+        itempat = r'(\s*(\w+)\s*:\s*(\D+\w+.\d+)\s*)'      
+
+        with open('reference_points.pb.txt') as f:
+            lod = [{group[1]:group[2] for group in re.findall(itempat, items)}
+                                        for items in re.findall(dictpat, f.read())]
+        self.df = pd.DataFrame(lod)
+        self.df = self.df.apply(pd.to_numeric)
+
+    def test_reference_spline(self):
+        spline = Cartesian2Fresnet.fr.reference_spline(self.df)
+        expected_y_value = 0.119615256786
+        self.assertEqual(spline.y[0],expected_y_value)
+
+
+        
     def test_current_s(self):
         """
         Test the current point
@@ -13,4 +35,5 @@ class TestFresnet(unittest.TestCase):
         self.assertEqual(result, 0.0)
 
 if __name__ == '__main__':
+
     unittest.main()
